84e710e36d3d1497bb884859c0d24c6394dab4c0
Yohan <yohan.g99@gmail.com>
2022-10-05 17:52:50 +0200

    fix auth routes

--- front/api/auth.js
+++ front/api/auth.js
@@ -1,12 +1,16 @@
 const BASE = "/auth";
 
 export default ($axios) => ({
-  async login() {
-    let res = await $axios.post(`${BASE}/login`);
+  async login(email, password) {
+    let res = await $axios.post(`${BASE}/login`, { login, password });
     return res.data;
   },
-  async register() {
-    let res = await $axios.post(`${BASE}/register`);
+  async register(pseudo, email, password) {
+    let res = await $axios.post(`${BASE}/register`, {
+      pseudo,
+      email,
+      password,
+    });
     return res.data;
   },
   async getMe() {
