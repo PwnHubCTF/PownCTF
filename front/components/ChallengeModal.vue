74f249791dfc00b896139917b5044420e46c6752
Eteckq <yohan.g99@gmail.com>
2022-10-31 15:24:07 +0100

    animation & fixs

--- front/components/ChallengeModal.vue
+++ front/components/ChallengeModal.vue
@@ -1,32 +1,29 @@
 <template>
-    <div
-      class="bg-gray-900 text-white p-5"
-      :class="{ 'bg-green-600': challenge.solved }"
-    >
-      <div class="items-center justify-center relative">
-        <div class="flex justify-center">
-          <h1 class="text-2xl">{{ challenge.name }}</h1>
-        </div>
-        <div class="flex justify-center">
-          <DifficultyStars class="my-2" :value="challenge.difficulty" />
-        </div>
-        <CommentButton
-          class="absolute -top-4 -right-4"
-          :challenge="challenge"
-        />
+  <div
+    class="bg-gray-900 text-white p-5"
+    :class="{ 'bg-green-600': challenge.solved }"
+  >
+    <div class="items-center justify-center relative">
+      <div class="flex justify-center">
+        <h1 class="text-2xl">{{ challenge.name }}</h1>
       </div>
-      <p class="text-gray-200 mt-4">
-        {{ challenge.description }}
-      </p>
-      <InputText
-        v-if="!challenge.solved"
-        class="text-gray-900"
-        type="text"
-        v-model="flag"
-        @enter="submitFlag"
-        placeholder="Flag"
-      />
+      <div class="flex justify-center">
+        <DifficultyStars class="my-2" :value="challenge.difficulty" />
+      </div>
+      <CommentButton class="absolute -top-4 -right-4" :challenge="challenge" />
     </div>
+    <p class="text-gray-200 mt-4">
+      {{ challenge.description }}
+    </p>
+    <InputText
+      v-if="!challenge.solved"
+      class="text-gray-900"
+      type="text"
+      v-model="flag"
+      @enter="submitFlag"
+      placeholder="Flag"
+    />
+  </div>
 </template>
 
 <script>
@@ -40,20 +37,34 @@ export default {
   methods: {
     async submitFlag() {
       if (this.flag == "") return;
-      let result = await this.$api.challenges.submit(
-        this.challenge.id,
-        this.flag
-      );
+      let result = null;
+      try {
+        result = await this.$api.challenges.submit(
+          this.challenge.id,
+          this.flag
+        );
+      } catch (error) {
+        this.$toast.error("Impossible to flag");
+      }
       this.$parent.$fetch();
-      if (result == "correct") {this.$toast.success("Good job!"); this.closeModal()}
-      else if (result == "incorrect") this.$toast.error("Wrong flag..");
-      else if (result == "solved") this.$toast.error("Challenge already flag");
+      switch (result) {
+        case "correct":
+          this.$toast.success("Good job!");
+          this.closeModal();
+          break;
+        case "incorrect":
+          this.$toast.error("Wrong flag..");
+          break;
+        case "solved":
+          this.$toast.error("Challenge already flag");
+          break;
+      }
 
       this.flag = "";
     },
-    closeModal(){
-      this.$emit('closeModal')
-    }
+    closeModal() {
+      this.$emit("closeModal");
+    },
   },
 };
 </script>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="flex">
     <Navbar class="h-screen shrink-0 scrollbar-thin overflow-y-scroll" />
-    <Nuxt class="w-full h-screen scrollbar-thin overflow-y-scroll" />
+    <transition name="page" mode="out-in">
+      <Nuxt class="w-full h-screen scrollbar-thin overflow-y-scroll" />
+    </transition>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
