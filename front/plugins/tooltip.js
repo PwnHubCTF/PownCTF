85d7a0974463dacd2ed90a996d82110932c0ced4
Eteckq <yohan.g99@gmail.com>
2023-01-26 16:43:20 +0100

    fix

--- front/plugins/tooltip.js
+++ front/plugins/tooltip.js
@@ -1,6 +1,4 @@
-import Vue from "vue"
-import { VTooltip, VPopover, VClosePopover } from "v-tooltip"
+import Vue from 'vue'
+import VTooltip from 'v-tooltip'
 
-Vue.directive("tooltip", VTooltip);
-Vue.directive("close-popover", VClosePopover)
-Vue.component("v-popover", VPopover)
+Vue.use(VTooltip)
