602a64d880b36b3ab2618ccdecda39af17115eba
Eteckq <yohan.g99@gmail.com>
2023-02-01 13:15:37 +0100

    ui

--- front/pages/admin/users.vue
+++ front/pages/admin/users.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-8">
+  <div class="p-8">
     <div class="overflow-x-auto relative">
       <table class="w-full text-sm text-left text-gray-800">
         <thead class="text-xs text-gray-700 uppercase bg-gray-400">
@@ -11,35 +11,34 @@
         <tbody>
           <tr
             class="border-b border-gray-200"
-            :class="[(index % 2 == 0) ? 'bg-gray-200' : 'bg-gray-300']"
+            :class="[index % 2 == 0 ? 'bg-gray-200' : 'bg-gray-300']"
             v-for="(user, index) in users"
             :key="index"
           >
-            <td class="py-4 px-6">{{user.pseudo}}</td>
-            <td class="py-4 px-6">{{user.email}}</td>
+            <td class="py-4 px-6">{{ user.pseudo }}</td>
+            <td class="py-4 px-6">{{ user.email }}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
-  </template>
-  
-  <script>
-  export default {
-    layout: "admin",
-    data(){
-        return {
-            users: []
-        }
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    testYo() {
+      return "yo";
     },
-    methods: {
-        testYo(){
-            return 'yo'
-        }
-    },
-    async beforeMount(){
-        this.users = await this.$api.users.getAdmin()
-    }
-  };
-  </script>
-  
+  },
+  async beforeMount() {
+    this.users = await this.$api.users.getAdmin();
+  },
+};
+</script>
