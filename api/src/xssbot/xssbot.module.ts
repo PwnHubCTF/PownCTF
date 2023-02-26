a170978f2041da5de8f203610801909ed9840fb5
Yohan <yohan.g99@gmail.com>
2023-02-26 14:45:41 +0100

    xss history

--- api/src/xssbot/xssbot.module.ts
+++ api/src/xssbot/xssbot.module.ts
@@ -1,13 +1,15 @@
 import { HttpModule } from '@nestjs/axios';
 import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChallengesModule } from 'src/challenges/challenges.module';
 import { ConfigsModule } from 'src/configs/configs.module';
 import { DeployerModule } from 'src/deployer/deployer.module';
+import { XSSSubmission } from './entities/xss-submission.entity';
 import { XSSBotController } from './xssbot.controller';
 import { XSSBotService } from './xssbot.service';
 
 @Module({
-  imports: [
+  imports: [TypeOrmModule.forFeature([XSSSubmission]),
     forwardRef(()=>ChallengesModule),
     DeployerModule,
     ConfigsModule,
