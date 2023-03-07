fd122e92103098b15859380ad35755880b208472
Eteckq <yohan.g99@gmail.com>
2023-03-07 17:27:28 +0100

    change theme position

--- front/components/admin/Navbar.vue
+++ front/components/admin/Navbar.vue
@@ -19,22 +19,6 @@
             </svg>
             <span class="ml-3">Back</span>
           </NuxtLink>
-          <NuxtLink to="/admin/theme" class="menuText group">
-            <svg
-              
-              aria-hidden="true"
-              class="menuIcon"
-              fill="currentColor"
-              width="24"
-              height="24"
-              viewBox="0 0 384 512"
-            >
-              <path
-                d="M192 64L160 0H128L96 64 64 0H48C21.5 0 0 21.5 0 48V256H384V48c0-26.5-21.5-48-48-48H224L192 64zM0 288v32c0 35.3 28.7 64 64 64h64v64c0 35.3 28.7 64 64 64s64-28.7 64-64V384h64c35.3 0 64-28.7 64-64V288H0zM192 464c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z"
-              />
-            </svg>
-            <span class="ml-3">Theme</span>
-          </NuxtLink>
           <NuxtLink to="/admin/config" class="menuText group">
             <svg
               
@@ -144,6 +128,22 @@
               />
             </svg>
             <span class="ml-3">Sockets</span>
+          </NuxtLink>
+          <NuxtLink to="/admin/theme" class="menuText group">
+            <svg
+              
+              aria-hidden="true"
+              class="menuIcon"
+              fill="currentColor"
+              width="24"
+              height="24"
+              viewBox="0 0 384 512"
+            >
+              <path
+                d="M192 64L160 0H128L96 64 64 0H48C21.5 0 0 21.5 0 48V256H384V48c0-26.5-21.5-48-48-48H224L192 64zM0 288v32c0 35.3 28.7 64 64 64h64v64c0 35.3 28.7 64 64 64s64-28.7 64-64V384h64c35.3 0 64-28.7 64-64V288H0zM192 464c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z"
+              />
+            </svg>
+            <span class="ml-3">Theme</span>
           </NuxtLink>
         </li>
       </ul>
