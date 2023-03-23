b46f39bcaa14d8ed446fbf0b25b26cc9e001de2a
Eteckq <yohan.g99@gmail.com>
2023-03-23 20:19:16 +0100

    template mail system

--- api/src/mail/mail.module.ts
+++ api/src/mail/mail.module.ts
@@ -1,5 +1,7 @@
 import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 import { Module } from '@nestjs/common';
+import * as path from 'path';
 import { ConfigsModule } from 'src/configs/configs.module';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
@@ -8,6 +10,13 @@ import { MailService } from './mail.service';
   imports: [
     MailerModule.forRoot({
       transport: process.env.MAIL_TRANSPORT,
+      template: {
+        dir: path.join(__dirname, '/../../mail_templates'),
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
     }),
     ConfigsModule
   ],
--- api/src/mail/mail.service.ts
+++ api/src/mail/mail.service.ts
@@ -5,24 +5,29 @@ import { ConfigsService } from 'src/configs/configs.service';
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService, private configService: ConfigsService){
+    constructor(private readonly mailerService: MailerService, private configService: ConfigsService) {
 
     }
 
-    async sendTest(){
+    async sendTest () {
         const nameCtf = await this.configService.getValueFromKey('ctf.event_name')
         try {
             const mail = await this.mailerService.sendMail(
                 {
-                    to: 'yohan.g99@gmail.com', // list of receivers
-                    from: `"${nameCtf}" <noreply@${nameCtf}.com>`, // sender address
-                    subject: 'Testing Nest MailerModule ✔', // Subject line
-                    text: 'welcome', // plaintext body
-                    html: '<b>welcome</b>', // HTML body content
-                  }
+                    to: 'yohan.g99@gmail.com',
+                    from: `"${nameCtf}" <noreply@${nameCtf}.com>`,
+                    subject: `Welcome on ${nameCtf}`,
+                    template: 'welcome',
+                    context: {
+                        name: 'NAME',
+                        confirmation_url: "URL",
+                    },
+                }
             )
             return mail
         } catch (error) {
+            console.log(error);
+
             throw new ForbiddenException('Mail server is not configured')
         }
     }
