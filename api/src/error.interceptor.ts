34871e13f6ecd5fd8d3aee21e8b4191c3565e73a
COTE Céleste <celeste@cote21.fr>
2023-05-06 13:50:50 +0200

    ignore some exeptions

--- api/src/error.interceptor.ts
+++ api/src/error.interceptor.ts
@@ -4,13 +4,21 @@ import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
 import * as Sentry from "@sentry/node";
 
+const ignoredMessage = [
+  "File not found",
+  "User not found / Incorrect password",
+  "Incorrect password"
+]
+
 @Injectable()
 export class ErroInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle()
       .pipe(catchError((err, caught) => {
-        console.log("test")
-        Sentry.captureException(err)
+        console.log(err.message)
+        if (!ignoredMessage.some(mess => mess == err.message)) {
+          Sentry.captureException(err)
+        }
         throws(err)
         return caught
       }));
